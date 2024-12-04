e5cd86a8684f03f40701c487c6b59f5e9a70e09a
rumeysatr <0704rumeysatatar@gmail.com>
2024-12-04 04:01:37 +0300

    ilk push

---
+++ library_otomation/Views/Library/addBook.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace library_otomation.Views.Library
+{
+    public class AddBookPage : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
