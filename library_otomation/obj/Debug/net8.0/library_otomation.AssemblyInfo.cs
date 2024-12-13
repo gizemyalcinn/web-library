38607b9895fab81c301bd9a50695831dd953984f
rumeysatr <0704rumeysatatar@gmail.com>
2024-12-14 01:21:49 +0300

    exit butonu eklendi ama hatalı çalışıyor

--- library_otomation/obj/Debug/net8.0/library_otomation.AssemblyInfo.cs
+++ library_otomation/obj/Debug/net8.0/library_otomation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("library_otomation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9240ca38b0b95039e24660cb87b388d5e578789")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("library_otomation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("library_otomation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
